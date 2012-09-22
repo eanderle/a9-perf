03e318da05b9628b18e7be4d9fd2be70f5beaf8c
Eric Anderle <eanderle@umich.edu>
2012-09-21 22:45:02 -0400

    added instruction boundries

--- a9_perf.c
+++ a9_perf.c
@@ -1,17 +1,25 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/delay.h>
 
 #define DRIVER_AUTHOR "Eric Anderle"
 #define DRIVER_DESC   "ARM Cortex A9 Performance Counters"
 #define DRIVER_VER    "0.1"
 
+#define RENAME_INST_EVENT 0x68
+
+long long accumulated_inst_count = 0;
+
 static inline void select_idx(int idx) {
     asm volatile("mcr p15, 0, %0, c9, c12, 5" : : "r" (idx));
+    asm volatile("isb");
 }
 
 static inline void enable_event_for_idx(int event, int idx) {
     select_idx(idx);
+    // Select event type
     asm volatile("mcr p15, 0, %0, c9, c13, 1" : : "r" (event));
+    // Count enable
     asm volatile("mcr p15, 0, %0, c9, c12, 1" : : "r" (0x1));
 }
 
@@ -25,14 +33,19 @@ static inline long long get_count_for_idx(int idx) {
 static int __init a9_perf_start(void)
 {
     printk("Cortex A9 Performance Counters version %s loaded\n", DRIVER_VER);
-    asm volatile("mcr p15, 0, %0, c9, c12, 0" : : "r" (0x1)); // Enable all counters
-    enable_event_for_idx(0x68, 0);
+    // Enable and clear all counters
+    asm volatile("mcr p15, 0, %0, c9, c12, 0" : : "r" (0x3));
+    // Clear overflow bits
+    asm volatile("mcr p15, 0, %0, c9, c12, 3" : : "r" (0x0));
+
+    enable_event_for_idx(RENAME_INST_EVENT, 0);
+
     return 0;
 }
 
 static void __exit a9_perf_end(void)
 {
-    printk("Instructions out of core renaming stage: %lld\n", get_count_for_idx(0));
+    printk("Instructions out of core renaming stage: %lld\n", accumulated_inst_count);
     printk("Cortex A9 Performance Counters version %s unloaded\n", DRIVER_VER);
 }
 
